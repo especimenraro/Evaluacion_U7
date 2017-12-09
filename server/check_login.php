d105ca259eff84cc9ea9012fd839d8e186024fe4
root <especimenraro.cl@gmail.com>
2017-12-09 00:52:01 -0300

    Check Login OK

--- server/check_login.php
+++ server/check_login.php
@@ -1,22 +1,46 @@
 <?php
-include "conector.php";
 
-$username = $_POST['username'];
+$username =$_POST['username'];
 $clave = $_POST['password'];
 
-//$agenda= new ConectorBD("localhost","agenda_php","agenda_php");
+
 $agenda = new mysqli("localhost","agenda_php","agenda_php","agenda");
-if($agenda->initConexion("agenda")=="OK") {
-	$usuarios = 	$agenda->consultar("usuarios","user");
-	echo $usuarios;
-}//FIN IF
-else { echo "error en la conexion";}
+
+if($agenda->errno) {
+	$response['msg']="Error de conexion con la BD:"." ".$agenda->error;
+	$agenda->close(); 	
+	}//FIN IF
+	
+	else {
+	$sql = "SELECT user,pass FROM usuarios WHERE user = '".$username."'";	
+	$usuarios = 	$agenda->query($sql);		
+	if ($usuarios->num_rows > 0) {
+      while($row = $usuarios->fetch_assoc()) {
+      	$response['msg']="OK";
+      if($row['pass']==$clave) {
+        	$response['msg']="OK";
+        	
+        } else {
+		   $response['msg']="Clave Incorrecta"; 
+		   
+        }
+    } // FIN WHILE
+   
+ 
+} // FIN IF 
+else {
+    $response['msg']="Usuario No Existe"; 
+		  
+		} // FIN ELSE
+	
+
+}// FIN ELSE
+
+ echo json_encode($response);    
 
 
 
 
-//$response['msg']="OK";
-//echo json_encode($response);
 
 
 
