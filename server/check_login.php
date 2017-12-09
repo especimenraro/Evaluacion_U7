fc17afac17bf4d9686a707862ed90c74f5e56a15
root <especimenraro.cl@gmail.com>
2017-12-08 23:41:07 -0300

    inicio de check login

--- server/check_login.php
+++ server/check_login.php
@@ -3,8 +3,20 @@ include "conector.php";
 
 $username = $_POST['username'];
 $clave = $_POST['password'];
-$response['msg']="OK";
-echo json_encode($response);
+
+//$agenda= new ConectorBD("localhost","agenda_php","agenda_php");
+$agenda = new mysqli("localhost","agenda_php","agenda_php","agenda");
+if($agenda->initConexion("agenda")=="OK") {
+	$usuarios = 	$agenda->consultar("usuarios","user");
+	echo $usuarios;
+}//FIN IF
+else { echo "error en la conexion";}
+
+
+
+
+//$response['msg']="OK";
+//echo json_encode($response);
 
 
 
